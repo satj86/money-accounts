70b7ea0071fc0bec8b10e8221bc6457f2f377d43
Satnam S Jootle <ssjootle@gmail.com>
2018-06-09 21:44:51 +0100

    circle config

---
+++ banana.tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace banana.tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
