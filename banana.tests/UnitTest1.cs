323bfb1918640bec456528bbd81fe87320d9bfff
= <=>
2018-06-19 01:12:41 +0100

    test tettings

--- banana.tests/UnitTest1.cs
+++ banana.tests/UnitTest1.cs
@@ -5,12 +5,18 @@ namespace banana.tests
     [TestClass]
     public class UnitTest1
     {
+        public TestContext TestContext { get; set; }
+        
+
         [TestMethod]
         public void Is_test_environment()
         {
-            var env = System.Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
 
+            var env = TestContext.Properties["environment"].ToString();
+
+            var secretKey = TestContext.Properties["secret"].ToString();
             Assert.AreEqual("Test", env);
+            Assert.AreEqual("shhhh", secretKey);
         }
     }
 }
