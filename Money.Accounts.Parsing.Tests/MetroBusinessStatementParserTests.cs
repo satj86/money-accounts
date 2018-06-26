d518f46187b472304c80fdff22088fc10478e4c7
= <=>
2018-06-26 07:27:46 +0100

    Add Metro Business Account statement parser

---
+++ Money.Accounts.Parsing.Tests/MetroBusinessStatementParserTests.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Money.Accounts.Parsing.Tests
+{
+    [TestClass]
+    public class MetroBusinessStatementParserTests
+    {
+        [TestMethod]
+        public void Metro_Business_parser_parses_statement_entries()
+        {
+            //Arrange
+            const string statemtentPath = "C:\\SampleStatements\\Transaction_24.06.2018.csv";
+            IStatementParser parser = new MetroBusinessStatementParser();
+
+            //Act
+            var statementEntries = parser.ReadStatement(statemtentPath);
+
+            //Assert
+            DateTime date;
+            DateTime.TryParse("22/06/2018", out date);
+
+            Assert.AreEqual(date, statementEntries.First().Date);
+            Assert.AreEqual("GRAVITAS RECRUITME", statementEntries.First().Description);
+            Assert.AreEqual(3103.44m, statementEntries.First().Amount);
+        }
+    }
+}
--- Money.Accounts.Parsing/MetroBusinessStatementParser.cs
+++ Money.Accounts.Parsing/MetroBusinessStatementParser.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using Money.Accounts.Parsing.Model;
 
@@ -9,7 +10,33 @@ namespace Money.Accounts.Parsing
     {
         public IEnumerable<StatementEntry> ReadStatement(string path)
         {
-            throw new NotImplementedException();
+            //Date,Reference,Transaction Type,Money In, Money Out,Balance
+
+            using (var reader = new StreamReader(path))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    if (line.Equals(string.Empty) || line.StartsWith("Date", StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        continue;
+                    }
+                    var values = line.Split(',');
+
+                    DateTime date;
+                    DateTime.TryParse(values[0], out date);
+
+                    var moneyIn = Convert.ToDecimal(values[3]);
+                    var moneyOut = Convert.ToDecimal(values[4]);
+
+                    yield return new StatementEntry
+                    {
+                        Date = date,
+                        Description = values[1],
+                        Amount = moneyIn > 0 ? moneyIn : -moneyOut
+                    };
+                }
+            }
         }
     }
 }
