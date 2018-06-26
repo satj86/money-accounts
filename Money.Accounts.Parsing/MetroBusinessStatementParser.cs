b572b88d34148d7d1e19853029aab692f8ffd4f9
= <=>
2018-06-26 07:15:10 +0100

    Add NatWest Current Account statement parser

---
+++ Money.Accounts.Parsing/MetroBusinessStatementParser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Money.Accounts.Parsing.Model;
+
+namespace Money.Accounts.Parsing
+{
+    public class MetroBusinessStatementParser : IStatementParser
+    {
+        public IEnumerable<StatementEntry> ReadStatement(string path)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
